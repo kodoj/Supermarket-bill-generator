06edb7cc057e0de649f29aeb1a812d72788662b2
kodoj <kodoj1@o2.pl>
2019-02-05 10:19:32 +0100

    you can now read codebars from file

--- src/main/java/com/codecool/BarcodeScanner.java
+++ src/main/java/com/codecool/BarcodeScanner.java
@@ -1,4 +1,26 @@
 package com.codecool;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class BarcodeScanner {
+
+    public List<String> readBarcodes(String fileName) {
+        List<String> barcodes = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()))) {
+            stream.forEach(barcodes::add);
+        } catch (Exception e){
+            if (e instanceof IOException || e instanceof URISyntaxException) {
+                e.printStackTrace();
+                System.out.println("No File Found");
+            }
+        }
+        return barcodes;
+    }
 }
--- src/test/java/com/codecool/BarcodeScannerTest.java
+++ src/test/java/com/codecool/BarcodeScannerTest.java
@@ -1,6 +1,9 @@
 package com.codecool;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BarcodeScannerTest {
@@ -8,8 +11,16 @@ public class BarcodeScannerTest {
     @Test
     void checkIfFirstBarcodeReadProperly() {
         BarcodeScanner barcodeScanner = new BarcodeScanner();
-        String[] barcodes = barcodeScanner.readBarcodes("barcodeList.txt");
+        List<String> barcodes = barcodeScanner.readBarcodes("barcodeList.txt");
         String expectedResult = "1001";
-        assertEquals(expectedResult, barcodes[0]);
+        assertEquals(expectedResult, barcodes.get(0));
+    }
+
+    @Test
+    void checkIfLastBarcodeReadProperly() {
+        BarcodeScanner barcodeScanner = new BarcodeScanner();
+        List<String> barcodes = barcodeScanner.readBarcodes("barcodeList.txt");
+        String expectedResult = "1243";
+        assertEquals(expectedResult, barcodes.get(9));
     }
 }
